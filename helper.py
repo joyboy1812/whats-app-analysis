531e0e9299dd113c5ad7a07fd5fcb0b477bfe067
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-09-03 19:08:59 +0530

    fixture

--- helper.py
+++ helper.py
@@ -13,18 +13,15 @@ def fetch_stats(selected_user,df):
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
 
-    # fetch the number of messages
+   
     num_messages = df.shape[0]
 
-    # fetch the total number of words
     words = []
     for message in df['message']:
         words.extend(message.split())
 
-    # fetch number of media messages
     num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
 
-    # fetch number of links shared
     links = []
     for message in df['message']:
         links.extend(extract.find_urls(message))
@@ -60,27 +57,6 @@ def create_wordcloud(selected_user, df):
     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
     return df_wc
 
-# def most_common_words(selected_user,df):
-
-#     f = open('stop_hinglish.txt','r')
-#     stop_words = f.read()
-
-#     if selected_user != 'Overall':
-#         df = df[df['user'] == selected_user]
-
-#     temp = df[df['user'] != 'group_notification']
-#     temp = temp[temp['message'] != '<Media omitted>\n']
-
-#     words = []
-
-#     for message in temp['message']:
-#         for word in message.lower().split():
-#             if word not in stop_words:
-#                 words.append(word)
-
-#     most_common_df = pd.DataFrame(Counter(words).most_common(20))
-#     return most_common_df
-
 
 
 def most_common_words(selected_user, df):
@@ -98,10 +74,10 @@ def most_common_words(selected_user, df):
     for message in temp['message']:
         for word in message.lower().split():
             word = word.strip()
-            # Skip if stopword
+            
             if word in stop_words:
                 continue
-            # Skip if it looks like a mobile number with @
+            
             if re.match(r"@\d{10,}", word):
                 continue
             words.append(word)
